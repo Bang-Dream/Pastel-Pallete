366e85d35b9cf96d2c39b437bebec48d40670ad5
J1min <jmhong101506@gmail.com>
2021-11-27 21:34:24 +0900

    :gear: :: (#0) Start
    슈키슈키

---
+++ README.md
@@ -0,0 +1,2 @@
+Node.js Express 라이브러리와
+React Typescript 를 사용합니다
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+});
