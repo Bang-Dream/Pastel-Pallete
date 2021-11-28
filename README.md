77bfdb1b8d2429008caabfb063188fa6955ccf46
J1min <jmhong101506@gmail.com>
2021-11-28 09:28:13 +0900

    :bug: :: (#1) 라우터 버그 수정, 404 페이지 추가
    .

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Node.js Express 라이브러리와
-React Typescript 를 사용합니다
+# 뱅드림 화이팅
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Test from "./components/Test";
+import Error from "./components/Error/Error";
+import Main from "./components/Main/Main";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="*" element={<Error />} />
+        <Route path="" element={<Main />} />
+        <Route path="test" element={<Test />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
