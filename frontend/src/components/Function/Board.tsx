9b09411b9d7f1ec2ceae2d95b1144aab520d941c
J1min <jmhong101506@gmail.com>
2021-12-01 21:46:58 +0900

    ⚡️ :: (#8) map함수가 안돌려지는 오류 고쳐주세요
    .

--- frontend/src/components/Function/Board.tsx
+++ frontend/src/components/Function/Board.tsx
@@ -1,42 +1,38 @@
-/* eslint-disable */
 import { useState, useEffect } from "react";
 import "../../scss/Reset.scss";
 import "../../scss/Main.scss";
 import { Link } from "react-router-dom";
 import axios from "axios";
 export default function Board() {
-  let [게시글, 게시글변경] = useState([
-    "학교 가기 싫다",
-    "뱅드림 하고싶다",
-    "왜 2교시는 사회야",
-  ]);
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  let [post, setPost] = useState(["게시글이 없습니다."]);
+  // const [users, setUsers] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
   useEffect(() => {
     axios
       .get("http://localhost:3001/api/list")
       .then((res) => {
         console.log(res.data);
-        // const [posts, setPosts] = useState([res.data]);
-        // console.log({ posts });
+        console.log(res.data[0].title);
+        console.log(res.data[0]);
+        setPost(res.data[0]);
       })
       .catch((err) => {
         console.log(err);
       });
     // .then(() => {
-    //   console.log("응애");
+    //   console.log("무족권실행되는것");
     // });
   }, []);
 
   return (
     <div className="center pt-5">
       {/* 여기를 바꿔야함  */}
-      {[1, 2, 3].map((a, i) => (
+      {post.map((a, i) => (
         <Link to="#" id="post" key={i}>
           <h4 style={{ marginTop: "70px" }}>
             {/* {만약 개추 > 10 이라면 념글 아이콘 보여주기} */}
-            {a} {게시글[i]}
+            {a}
             <span id="comment">(0)</span>
           </h4>
         </Link>
