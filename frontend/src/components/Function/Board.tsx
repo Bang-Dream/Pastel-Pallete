662252894a636e41d9ee5317c4cce304fe4a9679
J1min <jmhong101506@gmail.com>
2021-12-04 21:35:02 +0900

    ⚡️ :: (#10) 각종 일러스트 데이터 추가, 일러스트 페이지 완성
    .

--- frontend/src/components/Function/Board.tsx
+++ frontend/src/components/Function/Board.tsx
@@ -1,43 +1,37 @@
+/* eslint-disable */
 import { useState, useEffect } from "react";
 import "../../scss/Reset.scss";
 import "../../scss/Main.scss";
 import { Link } from "react-router-dom";
 import axios from "axios";
 export default function Board() {
-  let [post, setPost] = useState(["게시글이 없습니다."]);
-  // const [users, setUsers] = useState(null);
+  let [post, setPost] = useState([""]);
   // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(null);
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/api/list")
-      .then((res) => {
-        console.log(res.data);
-        console.log(res.data[0].title);
-        console.log(res.data[0]);
-        var copy = [...post];
-        copy.unshift(res.data[0].title);
-        setPost(copy);
-        // setPost(res.data[0].title);
-        // setPost(res.data[1].title); // .title
-        console.log("ok");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    // .then(() => {
-    //   console.log("무족권실행되는것");
-    // });
+    axios.get("http://localhost:3001/api/list").then((res) => {
+      var data = res.data;
+      var copy = [...post];
+
+      data.map((data: string, index: string): string | number =>
+        copy.unshift(res.data[index].title)
+      );
+      console.log(post);
+      console.log(copy);
+      setPost(copy);
+      // setPost(res.data[0].title);
+      // setPost(res.data[1].title); // .title
+      console.log("ok");
+    });
   }, []);
 
   return (
-    <div className="center pt-5">
+    <div className="center">
       {/* 여기를 바꿔야함  */}
-      {post.map((a, i) => (
-        <Link to="#" id="post" key={i}>
+      {post.map((data, index) => (
+        <Link to="#" id="post" key={index}>
           <h4 style={{ marginTop: "70px" }}>
             {/* {만약 개추 > 10 이라면 념글 아이콘 보여주기} */}
-            {a}
+            {data}
             <span id="comment">(0)</span>
           </h4>
           <hr />
--- frontend/src/components/Function/Illust.tsx
+++ frontend/src/components/Function/Illust.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import "../../scss/Reset.scss";
 import "../../scss/Main.scss";
+import data from "../Data/Data";
 export default function illust() {
-  return <div style={{ marginTop: "65px", marginLeft: "50px" }}> 일러스트</div>;
+  return (
+    <div style={{ marginTop: "65px", marginLeft: "50px" }}>
+      {data.map((data, index: number) => {
+        return (
+          <div key={index} style={{ marginTop: "30px", textAlign: "center" }}>
+            <img src={data.image} alt="" />
+            <h2>{data.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
