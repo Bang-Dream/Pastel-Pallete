0c9c22ae15de903204a6b000a69ce01ae8660f85
J1min <jmhong101506@gmail.com>
2021-12-15 00:13:47 +0900

    ⚡️ :: (#19) 삭제기능 수정
    .

--- frontend/src/components/Function/Board.tsx
+++ frontend/src/components/Function/Board.tsx
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import $ from "jquery";
 export default function Board() {
-  let [post, setPost] = useState(["이건 어케 없애는지 모를 기본 게시글"]);
-  let [id, setId] = useState([-1]);
+  let [post, setPost] = useState([]);
+  let [id, setId] = useState([]);
   // const [loading, setLoading] = useState(false);
   useEffect(() => {
     axios.get("http://localhost:3001/api/list").then((res) => {
@@ -15,35 +15,72 @@ export default function Board() {
       var titleCopy = [...post];
       var idCopy: number[] = id;
 
-      data.map((data: string, index: number): string | number =>
-        titleCopy.unshift(res.data[index].title)
-      );
+      data.map((data: string, index: number) => {
+        var titleData = res.data[index].title;
+        if (post !== null) {
+          titleCopy.unshift(titleData); // 'any' 형식의 인수는 'never' 형식의 매개 변수에 할당될 수 없습니다.
+        }
+      });
 
-      data.map((data: string, index: number): string[] | number[] | number =>
-        idCopy.unshift(res.data[index]._id)
-      );
+      data.map((data: string, index: number) => {
+        var idData = res.data[index]._id;
+        if (id !== null) {
+          idCopy.unshift(idData);
+        }
+      });
 
       console.log(idCopy);
       setPost(titleCopy);
-      setId(idCopy);
+      setId(idCopy); // 'number[]' 형식의 인수는 'SetStateAction<never[]>' 형식의 매개 변수에 할당될 수 없습니다.
       console.log("state setting ok");
     });
   }, []);
 
-  const deleteOne = (e: any) => {
-    $.ajax({
-      method: "DELETE",
-      url: "/delete",
-      data: { _id: e.target.dataset.id },
-    });
-  };
   const refreshOne = () => {
     window.location.reload();
   };
+
+  const deleteOne = (e: any) => {
+    
+    // $.ajax({
+    //   method: "DELETE",
+    //   url: "/delete",
+    //   data: { _id: e.target.dataset.id },
+    // });
+
+    // 위 코드를 axios로 어떻게 바꾸나요 아무리 예제를 찾아봐도 delete는 거의 안나와요 선생님
+    console.log(e.target.dataset.id);
+    axios
+      .delete("http://localhost:3001/api/list", { // 여기에 모든 데이터 다 json으로 잇서염
+        data: {
+          _id: e.target.dataset.id,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        console.log("ok");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // axios
+    //   .delete("/delete")
+    //   .then((res) => {
+    //     // refreshOne(); // 새로고침 함수
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
+  };
+
   return (
     <ul className="center">
       {post.map((data, index) => (
-        <li key={index}>
+        <form key={index} action="/delete" method="delete">
           <Link to="#" id="post">
             <h4 style={{ marginTop: "30px" }}>
               {/* {만약 개추 > 10 이라면 념글 아이콘 보여주기} */}
@@ -62,7 +99,7 @@ export default function Board() {
             </h4>
             <hr />
           </Link>
-        </li>
+        </form>
       ))}
       <Link to="/write">
         <button className="mt-5 mb-5 btn btn-primary">글쓰기</button>
